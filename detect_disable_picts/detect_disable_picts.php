44ee3caa12ddd561d5f6ec922b2d8dfd53471cca
deeeki <deeeki+gh@gmail.com>
2011-04-02 17:26:42 +0900

    fix uniqueness of picts

--- detect_disable_picts/detect_disable_picts.php
+++ detect_disable_picts/detect_disable_picts.php
@@ -5,10 +5,10 @@ $pict_contents = file_get_contents('picts.txt');
 $picts = explode("\n", $pict_contents);
 $picts_a = explode("\n", HTML_Emoji::getInstance('au')->filter($pict_contents, array('Output')));
 $picts_s = explode("\n", HTML_Emoji::getInstance('softbank')->filter($pict_contents, array('Output')));
-$disables_a = detect_disable_picts($picts_a);
-$disables_s = detect_disable_picts($picts_s);
+$disable_picts_a = detect_disable_picts($picts_a);
+$disable_picts_s = detect_disable_picts($picts_s);
 
-$disable_idxs = array_merge(array_keys($disables_a), array_keys($disables_s));
+$disable_idxs = array_unique(array_merge(array_keys($disable_picts_a), array_keys($disable_picts_s)));
 sort($disable_idxs);
 $disable_picts = array();
 foreach ($disable_idxs as $idx) {
@@ -23,6 +23,11 @@ function detect_disable_picts($picts = array()) {
 		}
 	}
 	return $disables;
+}
+
+if (isset($argv[0])) {
+	echo implode(' ', $disable_picts);
+	exit;
 }
 
 if ($emoji->isSjisCarrier()) {
