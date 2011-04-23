01ec77b28a7c496e74f79d98738968e263a34037
deeeki <deeeki+gh@gmail.com>
2011-04-24 01:24:56 +0900

    rename to bitly_util

--- bitly_util/example.php
+++ bitly_util/example.php
@@ -1,13 +1,13 @@
 <?php
 $url = 'http://php.net/';
 
-require_once('BitlyUtil.php');
+require_once('bitly_util.php');
 $short = BitlyUtil::shorten($url);
 $long = BitlyUtil::expand($short);
 echo (($url === $long) ? '[OK]' : '[NG]') . $url . "\n";
 echo $short . "\n" . $long . "\n";
 
-require_once('JmpUtil.php');
+require_once('jmp_util.php');
 $short = JmpUtil::shorten($url);
 $long = JmpUtil::expand($short);
 echo (($url === $long) ? '[OK]' : '[NG]') . $url . "\n";
