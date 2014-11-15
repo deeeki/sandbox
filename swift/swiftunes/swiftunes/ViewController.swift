47886c64447ce18e1fec4656758d12d0f1d00878
deeeki <deeeki@gmail.com>
2014-11-15 19:49:59 +0900

    Implement search itunes function

--- swift/swiftunes/swiftunes/ViewController.swift
+++ swift/swiftunes/swiftunes/ViewController.swift
@@ -22,6 +22,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+        var url: NSURL = NSURL(string: urlPath)
+        var request: NSURLRequest = NSURLRequest(URL: url)
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
+        
+        println("Search iTunes API at URL \(url)")
+        
+        connection.start()
+    }
 }
 
