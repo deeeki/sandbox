25c6e2f285837e7e14e69d12045849e09702b7cb
deeeki <deeeki@gmail.com>
2014-11-15 20:25:54 +0900

    Implement updating the table view ui

--- swift/swiftunes/swiftunes/ViewController.swift
+++ swift/swiftunes/swiftunes/ViewController.swift
@@ -65,5 +65,32 @@ class ViewController: UIViewController {
             self.appsTableView.reloadData()
         }
     }
+
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+
+        var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
+
+        cell.text = rowData["trackName"] as String
+
+        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        var imgURL: NSURL = NSURL(string: urlString)
+
+        // Download an NSData representation of the image at the URL
+        var imgData: NSData = NSData(contentsOfURL: imgURL)
+        cell.image = UIImage(data: imgData)
+
+        // Get the formatted price string for display in the subtitle
+        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+
+        cell.detailTextLabel.text = formattedPrice
+
+        return cell
+    }
 }
 
