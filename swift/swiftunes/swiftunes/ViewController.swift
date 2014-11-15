0f81b8b83b7ff60a477737091e3d395ca6a62591
deeeki <deeeki@gmail.com>
2014-11-15 19:24:50 +0900

    Initialize swiftunes profject
    http://ios-blog.co.uk/tutorials/developing-ios8-apps-using-swift-part-2-connect-to-the-itunes-search-api/
    http://jamesonquave.com/blog/developing-ios-apps-using-swift-tutorial-part-2/

---
+++ swift/swiftunes/swiftunes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftunes
+//
+//  Created by deeeki on 11/15/14.
+//  Copyright (c) 2014 deeeki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
