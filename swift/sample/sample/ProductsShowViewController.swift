21666692bf0ff7ac0e981c3077cd1f237d2f2ee3
deeeki <deeeki@gmail.com>
2015-02-26 01:01:49 +0900

    Add add button

--- swift/sample/sample/ProductsShowViewController.swift
+++ swift/sample/sample/ProductsShowViewController.swift
@@ -7,12 +7,24 @@ class ProductsShowViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = product
+        let addButton = UIButton(frame: CGRectMake(view.bounds.width - 100, 0, 100, 50))
+        addButton.setTitle("Add", forState: .Normal)
+        addButton.backgroundColor = UIColor.blueColor()
+        addButton.addTarget(self, action: "add:", forControlEvents: .TouchUpInside)
         let footerView = UIView(frame: CGRectMake(0, view.bounds.height - 50, view.bounds.width, 50))
         footerView.backgroundColor = UIColor.grayColor()
+        footerView.addSubview(addButton)
         view.addSubview(footerView)
+        let label = UILabel(frame: CGRectMake(0, 50, view.bounds.width, 50))
+        label.text = product
+        view.addSubview(label)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func add(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
     }
 }
