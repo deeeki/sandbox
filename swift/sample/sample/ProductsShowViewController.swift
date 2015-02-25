3f5010b131f453d8f5463d5032b3c129db9267a8
deeeki <deeeki@gmail.com>
2015-02-26 00:38:14 +0900

    Add footer

--- swift/sample/sample/ProductsShowViewController.swift
+++ swift/sample/sample/ProductsShowViewController.swift
@@ -7,6 +7,9 @@ class ProductsShowViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = product
+        let footerView = UIView(frame: CGRectMake(0, view.bounds.height - 50, view.bounds.width, 50))
+        footerView.backgroundColor = UIColor.grayColor()
+        view.addSubview(footerView)
     }
     
     override func didReceiveMemoryWarning() {
