db0a46b60e263a7e1df629cf0eeafc122febabdc
deeeki <deeeki@gmail.com>
2015-02-25 20:16:33 +0900

    Create new single page application

---
+++ swift/sample/sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by deeeki on 2/25/15.
+//  Copyright (c) 2015 deeeki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
