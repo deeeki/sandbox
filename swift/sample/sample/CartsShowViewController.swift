f6ed9b6eabd1c68502283544f07e485abcd48e00
deeeki <deeeki@gmail.com>
2015-02-27 12:18:32 +0900

    Show cart using TableView

--- swift/sample/sample/CartsShowViewController.swift
+++ swift/sample/sample/CartsShowViewController.swift
@@ -1,13 +1,34 @@
 import UIKit
 
-class CartsShowViewController: UIViewController {
+class CartsShowViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var products: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "cart"
+
+        let defaults = NSUserDefaults.standardUserDefaults()
+        products = defaults.objectForKey("cart") as [String]
+
+        let tableView = UITableView(frame: view.frame)
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        view.addSubview(tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        cell.textLabel.text = products[indexPath.row]
+        return cell
     }
 }
