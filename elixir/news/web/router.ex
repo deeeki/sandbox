ab5b39cac9cd018828b07c6820dbb28b51d25756
deeeki <deeeki@gmail.com>
2015-06-18 17:27:36 +0900

    Route to articles

--- elixir/news/web/router.ex
+++ elixir/news/web/router.ex
@@ -16,6 +16,7 @@ defmodule News.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/articles", ArticleController
   end
 
   # Other scopes may use custom stacks.
